eebde03130448e883a05af3012349c873d72ca69
Shubham <sdawasare1@gmail.com>
2022-02-02 19:41:16 +0530

    Model Change

--- Django1/api/models.py
+++ Django1/api/models.py
@@ -10,5 +10,6 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
+    token = models.IntegerField()
 
  
